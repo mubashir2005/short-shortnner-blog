206a808608b4c7b19650909e57a97a9ff4769e61
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-11-07 21:37:56 +0530

    fixed title

--- pages/newsletter.jsx
+++ pages/newsletter.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Home from "../components/home";
+import Head from "next/head";
 
 function newsletter() {
   return (
     <Home>
+      <Head>
+        <title>Newsletter</title>
+      </Head>
+
       <h3>Sign up for our Newsletter</h3>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSdDInWI-9rsqe119VVQkzQTJF7LkW6knLz26VjnczAFL4jPbQ/viewform?embedded=true"
