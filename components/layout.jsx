107c11f443a92db6cab5905ad70eaf6bc7c19b9b
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-19 20:41:34 +0530

    fixed

--- components/layout.jsx
+++ components/layout.jsx
@@ -7,60 +7,74 @@ const name = "Mohammed Mubashir Hasan";
 export const siteTitle = "Mubashir's Blog";
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="./logo.svg" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
+    return ( <
+        div className = { styles.container } >
+        <
+        Head >
+        <
+        link rel = "icon"
+        href = "./logo.svg" / >
+        <
+        meta name = "description"
+        content = "Creator and founder" / >
+        <
+        meta property = "og:image"
+        content = { `https://og-image.now.sh/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.png"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.png"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg` }
+        /> <
+        meta name = "og:title"
+        content = { siteTitle }
+        /> <
+        meta name = "twitter:card"
+        content = "summary_large_image" / >
+        <
+        /Head> <
+        header className = { styles.header } > {
+            home ? ( <
+                >
+                <
+                img src = "/images/profile.png"
+                className = { `${styles.headerHomeImage} ${utilStyles.borderCircle}` }
+                alt = { name }
+                /> <
+                h1 className = { utilStyles.heading2Xl } > { name } < /h1> <
                 />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="main/">
-            <a>← See my other blogs</a>
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+            ) : ( <
+                >
+                <
+                Link href = "/" >
+                <
+                a >
+                <
+                img src = "/images/profile.png"
+                className = { `${styles.headerImage} ${utilStyles.borderCircle}` }
+                alt = { name }
+                /> <
+                /a> <
+                /Link> <
+                h2 className = { utilStyles.headingLg } >
+                <
+                Link href = "/" >
+                <
+                a className = { utilStyles.colorInherit } > { name } < /a> <
+                /Link> <
+                /h2> <
+                />
+            )
+        } <
+        /header> <
+        main > { children } < /main> {
+            !home && ( <
+                div className = { styles.backToHome } >
+                <
+                Link href = "main/" >
+                <
+                a > ←See my other blogs < /a> <
+                /Link> <
+                /div>
+            )
+        } <
+        /div>
+    );
 }
